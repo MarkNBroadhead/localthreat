aa0098ba59ccd40056a2ce8e2e6e261bdf7d290a
Mark N Broadhead <MarkNBroadhead@gmail.com>
2024-01-15 23:51:31 -0700

    WIP

---
+++ client/src/lib/cache.ts
@@ -0,0 +1,31 @@
+export class ApiCache {
+
+  isLocalStorageAvailable = (): boolean =>  {
+    var test = 'test';
+    try {
+      localStorage.setItem(test, test);
+      localStorage.removeItem(test);
+      return true;
+    } catch(e) {
+      return false;
+    }
+  }
+
+  public set = (id: string, value: string) => {
+    if (this.isLocalStorageAvailable()) {
+      localStorage.setItem(id, value);
+    }
+  }
+
+  public get = (id: string) => {
+    if (this.isLocalStorageAvailable()) {
+      let value = localStorage.getItem(id);
+      if (!!value) {
+        return JSON.parse(value);
+      }
+    }
+  }
+
+  public recordExists = () => {
+  }
+}
