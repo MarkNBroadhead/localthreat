7ff581b212b2e3024b860bda7401aa8ca1ad68ca
Mark N Broadhead <MarkNBroadhead@gmail.com>
2024-01-16 00:04:41 -0700

    Rework schema initialization

--- api/schema.sql
+++ api/schema.sql
@@ -1,7 +1,10 @@
-CREATE TABLE IF NOT EXISTS reports (
-	id text NOT NULL PRIMARY KEY,
-	data text[]
-);
+CREATE SCHEMA IF NOT EXISTS postgres
+    AUTHORIZATION postgres;
 
-ALTER TABLE reports ADD IF NOT EXISTS time timestamptz NOT NULL DEFAULT now();
-
+CREATE TABLE IF NOT EXISTS postgres.reports
+(
+    id text COLLATE pg_catalog."default" NOT NULL,
+    data text[] COLLATE pg_catalog."default",
+    "time" timestamp with time zone NOT NULL DEFAULT now(),
+    CONSTRAINT reports_pkey PRIMARY KEY (id)
+)
